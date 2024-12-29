7e4d5a88d202117022ea5eaaeb84fdb10c192e07
phuongnnhi <85156989+phuongnnhi@users.noreply.github.com>
2024-12-29 22:55:21 +0100

    merge back and front end

--- coderdex-be/server.js
+++ coderdex-be/server.js
@@ -9,9 +9,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000; // Use default port if not specified in .env
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors()); // Enable cross-origin resource sharing
@@ -20,3 +18,11 @@ const pokedexRoutes = require('./routes/pokedex-api')
 app.use('/pokemons', pokedexRoutes);
 
 app.use('/images', express.static('images', express.static(path.join(__dirname, '../coderdex-fe/public/images')))); // Serve static images
+// Catch-all route for React front-end (if React app is served)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
